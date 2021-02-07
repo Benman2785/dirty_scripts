8b5ef4567dcae49dd2ce73f705e1d9131961a27d
Benman2785 <benman2785@hotmail.de>
2021-02-08 00:49:38 +0100

    Delete priovm.sh

--- priovm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PR1=`pidof VBoxHeadless`
-for p in `ls /proc/$PR1/task`; do taskset -p -c 0 $p; done
-ls /proc/$PR1/task | xargs renice -19
-echo "core affinity and priority set successfull"
